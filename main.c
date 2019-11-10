50666c24452eabd6bdfbbdba74ca26f584d81869
Robert J. Bonagura <rjb348@butter.cs.rutgers.edu>
2019-11-10 18:11:05 -0500

    Add comments

--- main.c
+++ main.c
@@ -5,9 +5,11 @@
 
 int main(int argc, char **argv) {
 
+	/* These are just some preset values for running a simple test case. */
 	int n = 10;
 	int value = 5;
 
+	/* Print list of scrambled numbers, search for value, and then swap this value with a randomly chosen index. */
 	printf("The list is:\n");
 	int* list = listGen(n);
 	int indexOfValue = findVal(list, n, value);
