4b62d937f695804bc45902a7a71de13a831edf46
Robert J. Bonagura <rjb348@butter.cs.rutgers.edu>
2019-11-14 19:46:01 -0500

    Fork a child process to search the array

--- proc.c
+++ proc.c
@@ -2,14 +2,29 @@
 
 int search(int* list, int n, int val){
 
-	printf("Proc search called\n");
-
-	int i = 0;
-	for (i; i < n; i++){
-		if (list[i] == val){
-			return i;
+	pid_t childPid = fork();
+	if (childPid == 0){
+		//Child
+		int i = 0;
+		for (i; i < n; i++){
+			if (list[i] == val){
+				printf("Child says %d\n", i);
+				exit(i);
+			}
 		}
 	}
+	else if (childPid < 1){
+		printf("Fork failed"); // Log error
+	} else {
+		//Parent
+		int childResult = -1;
+		wait(&childResult);
+		childResult = childResult / 255;
+		return childResult;
+	}
+
+	printf("Proc search called\n");
+
 
 	return -1;
 
--- proc.h
+++ proc.h
@@ -1,5 +1,6 @@
 #ifndef _PROC_H
 #define _PROC_H
+#include <unistd.h>
 #include "helper.h"
 
 #define _search(list, n, value) search((list), (n), (value))
