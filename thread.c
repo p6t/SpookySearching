7fed0d937bec1c507af689d8cf922cffbd2d8dfb
Robert J. Bonagura <rjb348@butter.cs.rutgers.edu>
2019-11-14 19:52:10 -0500

    Added comments to Thread's search function

--- thread.c
+++ thread.c
@@ -1,16 +1,17 @@
 #include "thread.h"
 
+/* Creation of child threads will be called inside search function. */
 int search(int* list, int n, int val){
 
 	printf("Thread search called\n");
-
+	
+	// This block of code is what would be called by the child thread.	
 	int i = 0;
 	for (i; i < n; i++){
 		if (list[i] == val){
 			return i;
 		}
 	}
-
+	
 	return -1;
-
 }
