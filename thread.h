7a576982f890c5ddd41618754719d726036303e2
Peter E. Tilton <pet27@python.cs.rutgers.edu>
2019-11-15 23:04:11 -0500

    wrote thread code, modified helper and main code

--- thread.h
+++ thread.h
@@ -1,9 +1,21 @@
 #ifndef _THREAD_H
 #define _THREAD_H
 #include "helper.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-#define _search(list, n, value) search((list), (n), (value))
+#define _search(list, size, parallels, searches) thread_search((list), (size), (parallels), (searches))
 
-int search(int* list, int n, int val);
+typedef struct _thread_atts {
+	int* list;
+	int list_size;
+	int target;
+	int result;
+} thread_atts;
+
+int thread_search(int* list, int list_size, int n_threads, int target);
+void* my_thread(void* arg);
+int linear_search(int* list, int list_size, int target);
 
 #endif
