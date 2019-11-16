5e4ac9b7c902c151696c03d35e26b3a650d5b516
Peter E. Tilton <pet27@python.cs.rutgers.edu>
2019-11-15 23:04:11 -0500

    wrote thread code, modified helper and main code

--- helper.c
+++ helper.c
@@ -28,12 +28,12 @@ int* listGen(int n){
 	return list;
 }
 
-int _search(int* list, int n, int val){
+int _search(int* list, int list_size, int parallels, int target){
 
 	printf("Original findVal function called\n");
 	int i = 0;
-	for (i; i < n; i++){
-		if (list[i] == val){
+	for (i; i < list_size; i++){
+		if (list[i] == target){
 			return i;
 		}
 	}
@@ -51,6 +51,42 @@ void modifyList(int* list, int n, int index){
 	
 	list[randNum] = val;
 	list[index] = element;
+}
+
+/*
+split_list(): breaks up a list into parts to give to processes or threads.
+--
+int list_size: number of elements in list.
+int* start_i: int array of size n_parallels. Start this as an array of all zeroes in the heap.
+int n_parallels: number of parts to split the list into.
+--
+start_i will be modified so each element is an evenly spaced start index for parallel searching.
+for example, calling split_list(8, [0,0], 2) will change [0,0] into [0,4]. 
+int return: 0 if success, -1 if error.
+*/
+int split_list(int list_size, int* start_i, int n_parallels) {
+	int i, err1, err2;
+	if(n_parallels == 0) {
+		return(-1);
+	}
+	if(n_parallels == 1) {
+		return(0);
+	}
+	if(n_parellels == 2) {
+		start_i[1] += list_size/2;
+		return(0);
+	}
+
+	// Recursively adding to the start indicies.
+	for(i=n_parallels/2; i<n_parallels; ++i) {
+		start_i[i] += list_size/2;
+	}
+	err1 = split_list(list_size/2, start_i, n_parallels/2));
+	err2 = split_list(list_size - list_size/2, &(start_i[n_parallels/2], n_parallels - n_parallels/2);
+	if(err1 || err2) {
+		return(-1);
+	}
+	return(0);
 }
 
 
--- thread.c
+++ thread.c
@@ -1,19 +1,103 @@
 #include "thread.h"
 
-// Made this comment to create a new branch on github.
+/*
+thread_search(): a driver to do multiple searches on a randomized list using multithreading.
+--
+int* list: array of randomized integers.
+int list_size: number of elements in list.
+int n_threads: number of threads to use for search.
+int target: the number to find in the list.
+--
+int return: the index of the target in list, or -1 if not found.
+*/
+int thread_search(int* list, int list_size, int n_threads, int target){
+	int i;
+	int err;
+	int* start_i;
+	pthread_t thread[n_threads];
+	thread_atts* atts[n_threads];
+	int thread_returns[n_threads];
+	
+	// Getting start indicies for each thread.
+	start_i = malloc(n_threads*sizeof(int));
+	for(i=0; i<n_threads; ++i) {
+		start_i[i] = 0;
+	}
+	split_list(list_size, start_i, n_threads);
 
-/* Creation of child threads will be called inside search function. */
-int search(int* list, int n, int val){
-	
-	printf("Thread search called\n");
-	
-	// This block of code is what would be called by the child thread.	
-	int i = 0;
-	for (i; i < n; i++){
-		if (list[i] == val){
-			return i;
+	// Creating attribute structs for each thread.
+	for(i=0; i<n_threads; ++i) {
+		atts[i] = malloc(sizeof(thread_atts));
+		atts[i]->list = &(list[start_i[i]]);
+		if(i == n_threads-1) {
+			atts[i]->list_size = list_size-start_i[i];
+		}
+		else {
+			atts[i]->list_size = start_i[i+1]-start_i[i];
+		}
+		atts[i]->target = target;
+	}
+
+	// Creating a number of threads running linear_search() on parts of the list.
+	for(i=0; i<n_threads; ++i) {
+		err = pthread_create(&thread[i], NULL, my_thread, (void*) atts[i])
+		
+		// If thread creation failed.
+		if(err) {
+			return(-1);
 		}
 	}
+
+	// Joining the threads back together.
+	for(i=0; i<n_threads; ++i) {
+		pthread_join(thread[i], NULL);
+		thread_returns[i] = atts[i]->result;
+	}
+
+	// Freeing allocated memory.
+	free(start_i);
+	for(i=0; i<n_threads; ++i) {
+		free(atts[i]);
+	} 
 	
-	return -1;
+	// Inspecting the return values of each thread for a success.
+	for(i=0; i<n_threads; ++i) {
+		if(thread_returns[i] != -1) {
+			return(thread_returns[i]);
+		}
+	}
+	return(-1);
 }
+
+void* my_thread(void* arg) {
+	thread_atts *tdata = (thread_atts*)arg;
+	
+	int* list = tdata->list;
+	int list_size = tdata->list_size;
+	int target = tdata->target;
+
+	int result = linear_search(list, list_size, target);
+	pthread_exit(NULL);
+}
+
+/*
+linear_search(): a linear search of an int array.
+--
+int* list: array of integers.
+int list_size: number of elements in list.
+int target: the number to find in list.
+--
+int return: the index of the target in list, or -1 if not found.
+*/
+int linear_search(int* list, int list_size, int target) {
+        int i;
+        for(i=0; i<list_size; ++i) {
+                if(list[i] == val) {
+                        return(i);
+                }
+        }
+        return(-1);
+}
+
+
+
